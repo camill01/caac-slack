60bb70e1c540a93c8b559fc93e2347306ed2fb84
William Kammersell <william.kammersell@ca.com>
2016-07-01 11:11:20 -0600

    Hello World to Slack

--- web.js
+++ web.js
@@ -1,5 +1,6 @@
 var express = require('express');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+var https = require('https');
 
 var app = express();
 //app.set('port', (process.env.PORT || 5000));
@@ -10,22 +11,74 @@ console.log( "Initialized" );
 
 // POST /test gives hello world to console
 app.post('/test', jsonParser, function (req, res) {
-  console.log( "Hello World!" );
+/*	var slackJSON = {
+		text: "This is a line of text in a channel.\nAnd this is another line of text."
+	};
+	
+	console.log( JSON.stringify( slackJSON ) );
+	
+	var options = {
+   		hostname: "hooks.slack.com",
+   		path: "/services/T1N5XSJVA/B1N4TJEJF/DfvcOqV4Xbedxi6BXsWJYflU",
+   		method: "POST"//,
+   	//	headers: {
+   //     	"Content-Type": "application/json"
+	//    },
+	    body: JSON.stringify( slackJSON )
+ 	};
+ 		
+	var remote_request = https.request(options, function(remote_response) {
+		console.log( remote_response);
+		remote_response.setEncoding("utf-8");
+		var responseString = "";
+		remote_response.on("data", function(data) {
+	  		responseString += data;
+	  		console.log(responseString);
+	 	});
+	 	remote_response.on("end", function() {
+	  		var resultObject = JSON.parse(responseString);
+			console.log(resultObject);
+			res.send(resultObject);
+		});
+	 });
+	remote_request.end();
+	
+	remote_request.on('error', (e) => {
+  		console.error(e);
+	});*/
+	
+	var options = {
+	    hostname : 'hooks.slack.com' ,
+	    path     : '/services/T1N5XSJVA/B1N4TJEJF/DfvcOqV4Xbedxi6BXsWJYflU' ,
+	    method   : 'POST'
+	};
+
+	var payload1 = {
+    //	"channel"    : "test" ,
+    //	"username"   : "masterbot" ,
+    	"text"       : "Hello from Nodejs!"// ,
+    //	"icon_emoji" : ":ghost:"
+	};
+
+	var req = https.request( options , function (res , b , c) {
+    	res.setEncoding( 'utf8' );
+    	res.on( 'data' , function (chunk) {
+    	} );
+	} );
+
+	req.on( 'error' , function (e) {
+   		console.log( 'problem with request: ' + e.message );
+	} );
+
+	req.write( JSON.stringify( payload1 ) );
+	req.end();
+	
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
 	console.log( "Listening on " + port);
 });
-
-
-/*app.post("/", function(req, res) {
-	console.log(req.body);
-});
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-	console.log("Listening on " + port);
-});*/
 
 /*
 var express = require(“express”);
