877bef2837d01bd4863fb092c82eca2ec5234141
William Kammersell <william.kammersell@ca.com>
2016-07-05 11:57:30 -0600

    Try Error again

--- web.js
+++ web.js
@@ -92,7 +92,7 @@ app.get('/slackauth', jsonParser, function (req, res) {
     	resOAuth.setEncoding( 'utf8' );
     	resOAuth.on('data', (d) => {
     		console.log( d );
-    		console.log ( d."ok" );
+    		console.log ( d["ok"] );
     		if ( d.ok == false ) {
     			return new Error("Error with Slack Response: " + d);
     		}
