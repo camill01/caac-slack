205ca341918f9166284fba91c53ab7801c6af4d5
William Kammersell <william.kammersell@ca.com>
2016-07-05 11:59:00 -0600

    Parse to JSON

--- web.js
+++ web.js
@@ -92,8 +92,9 @@ app.get('/slackauth', jsonParser, function (req, res) {
     	resOAuth.setEncoding( 'utf8' );
     	resOAuth.on('data', (d) => {
     		console.log( d );
-    		console.log ( d["ok"] );
-    		if ( d.ok == false ) {
+    		var data = JSON.parse(d);
+    		console.log ( data.ok );
+    		if ( data.ok == false ) {
     			return new Error("Error with Slack Response: " + d);
     		}
     		teamId = d.team_id;
