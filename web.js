674444ea6a468f25401e0e6b6b7cb1d1281e0dec
William Kammersell <william.kammersell@ca.com>
2016-07-05 10:04:49 -0600

    First attempt at OAuth

--- web.js
+++ web.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var https = require('https');
+var slackClientId = process.env.SLACK_CLIENT_ID;
+var slackClientSecret = process.env.SLACK_CLIENT_SECRET;
 
 var app = express();
 //app.set('port', (process.env.PORT || 5000));
@@ -51,6 +53,47 @@ app.post('/caacnotify', jsonParser, function (req, res) {
 
 	req.write( JSON.stringify( payload1 ) );
 	req.end();
+	res.end();
+});
+
+/* Endpoint for Slack in the OAuth flow */
+app.get('/slackauth', jsonParser, function (req, res) {
+	console.log('Slack Auth starting...');
+	
+	var temporaryCode = req.query.code;
+	console.log('Received temporary code');
+	
+	var options = {
+	    hostname : 'slack.com' ,
+	    path     : '/api/oauth.access',
+	    method   : 'POST'
+	};
+
+	var payload = {
+    	"client_id" : slackClientId,
+    	"client_secret" : slackClientSecret,
+    	"code" : temporaryCode,
+    	"redirect_uri" : "https://lower-donair-82094.herokuapp.com/slackauth"
+	};
+	
+	var req = https.request( options , res => {
+		console.log("Response from Slack");
+		console.log('statusCode: ', res.statusCode);
+		
+    	res.setEncoding( 'utf8' );
+    	res.on('data', (d) => {
+    		console.log( res );
+  		});
+	} );
+
+	req.on( 'error' , function (e) {
+   		console.log( 'problem with request: ' + e.message );
+	} );
+
+	req.write( JSON.stringify( payload ) );
+	req.end();
+	
+	console.log('Slack Auth done...');
 	res.end();
 });
 
